39f7cea8c61947486d59f23423954acdf1c8d6ad
Goran <goran@datacamp.com>
2018-02-23 16:12:39 +0100

    Handle smpp connection

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// TODO
+const smpp = require("smpp");
+const randomize = require("randomatic");
+
+const server = smpp.createServer(function(session) {
+  session.on("bind_transceiver", function(pdu) {
+    console.log("connected", pdu);
+  });
+
+  session.on("submit_sm", function(pdu) {
+    session.send(
+      pdu.response({
+        message_id: randomize("a", 10)
+      })
+    );
+  });
+});
+
+server.listen(2775);
