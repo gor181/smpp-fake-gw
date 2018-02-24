c19dad780f37ddce2e10077454100947a7c5bee8
Goran <goran@datacamp.com>
2018-02-24 22:17:31 +0100

    Add session listeners

--- index.js
+++ index.js
@@ -3,16 +3,30 @@ const randomize = require("randomatic");
 
 const server = smpp.createServer(function(session) {
   session.on("bind_transceiver", function(pdu) {
-    console.log("connected", pdu);
+    console.log(`User ${pdu.system_id} connected`);
+    session.send(pdu.response());
   });
 
   session.on("submit_sm", function(pdu) {
+    const messageId = randomize("a", 10);
     session.send(
       pdu.response({
-        message_id: randomize("a", 10)
+        message_id: messageId
       })
     );
   });
+
+  session.on("enquire_link", function(pdu) {
+    session.send(pdu.response());
+  });
+
+  session.on("error", function(pdu) {
+    console.error(error);
+  });
+
+  session.on("close", function(pdu) {
+    console.log("connection closed");
+  });
 });
 
-server.listen(2775);
+server.listen(2875, "localhost");
